80cdd16bf4d888c94dde3b74feb664d3f1bb5f0c
Francisco Ruiz <franciscoruizlezcano@gmail.com>
2021-03-08 14:45:29 -0300

    Update accounts.sql

--- src/accounts/main/resources/database/accounts.sql
+++ src/accounts/main/resources/database/accounts.sql
@@ -1,70 +1,131 @@
-CREATE TABLE IF NOT EXISTS modules (
-    id   CHAR(36)    NOT NULL,
-    name VARCHAR(45) NOT NULL,
-    PRIMARY KEY (id)
-)
-    ENGINE = InnoDB
-    DEFAULT CHARSET = utf8mb4
-    COLLATE = utf8mb4_unicode_ci;
+-- MySQL dump 10.13  Distrib 8.0.23, for Linux (x86_64)
+--
+-- Host: localhost    Database: accounts
+-- ------------------------------------------------------
+-- Server version	8.0.23
 
-CREATE TABLE IF NOT EXISTS actions (
-    id        CHAR(36)    NOT NULL,
-    name      VARCHAR(45) NOT NULL,
-    module_id CHAR(36)    NOT NULL,
-    PRIMARY KEY (id),
-	constraint fk_actions__modules FOREIGN KEY (module_id) REFERENCES modules (id)
-)
-    ENGINE = InnoDB
-    DEFAULT CHARSET = utf8mb4
-    COLLATE = utf8mb4_unicode_ci;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE TABLE IF NOT EXISTS roles (
-    id   CHAR(36)    NOT NULL,
-    name VARCHAR(45) NOT NULL,
-    PRIMARY KEY (id)
-)
-    ENGINE = InnoDB
-    DEFAULT CHARSET = utf8mb4
-    COLLATE = utf8mb4_unicode_ci;
+--
+-- Table structure for table `actions`
+--
 
-CREATE TABLE IF NOT EXISTS role_actions (
-    role_id   CHAR(36) NOT NULL,
-    action_id CHAR(36) NOT NULL,
-	PRIMARY KEY (role_id, action_id),
-	CONSTRAINT fk_role_actions__roles FOREIGN KEY (role_id) REFERENCES roles (id),
-	CONSTRAINT fk_role_actions__actions FOREIGN KEY (action_id) REFERENCES actions (id)
-)
-    ENGINE = InnoDB
-    DEFAULT CHARSET = utf8mb4
-    COLLATE = utf8mb4_unicode_ci;
+DROP TABLE IF EXISTS `actions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `actions` (
+  `id` char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `method` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `module_id` char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_actions__modules` (`module_id`),
+  CONSTRAINT `fk_actions__modules` FOREIGN KEY (`module_id`) REFERENCES `modules` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE IF NOT EXISTS users (
-    id                CHAR(36)     NOT NULL,
-    username          VARCHAR(320) NOT NULL,
-    email             VARCHAR(320) NOT NULL,
-    password          VARCHAR(320) NOT NULL,
-    name              VARCHAR(45)  NOT NULL,
-    lastname          VARCHAR(45)  NOT NULL,
-    phone             VARCHAR(25)  NOT NULL,
-    profile_photo_url TEXT         NOT NULL,
-    created_date      DATETIME     NOT NULL,
-    updated_date      DATETIME     NOT NULL,
-    deleted_date      DATETIME     NOT NULL,
-    role_id           CHAR(36)     NOT NULL,
-    PRIMARY KEY (id),
-	CONSTRAINT fk_users__roles FOREIGN KEY (role_id) REFERENCES roles (id)
-)
-    ENGINE = InnoDB
-    DEFAULT CHARSET = utf8mb4
-    COLLATE = utf8mb4_unicode_ci;
+--
+-- Table structure for table `modules`
+--
 
-CREATE TABLE IF NOT EXISTS sessions (
-    id      CHAR(36) NOT NULL,
-    token   TEXT     NOT NULL,
-    user_id CHAR(36) NOT NULL,
-    PRIMARY KEY (id),
-    CONSTRAINT fk_sessions__users FOREIGN KEY (user_id) REFERENCES users (id)
-)
-    ENGINE = InnoDB
-    DEFAULT CHARSET = utf8mb4
-    COLLATE = utf8mb4_unicode_ci;
+DROP TABLE IF EXISTS `modules`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `modules` (
+  `id` char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `endpoint` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `role_actions`
+--
+
+DROP TABLE IF EXISTS `role_actions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `role_actions` (
+  `role_id` char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `action_id` char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`role_id`,`action_id`),
+  KEY `fk_role_actions__actions` (`action_id`),
+  CONSTRAINT `fk_role_actions__actions` FOREIGN KEY (`action_id`) REFERENCES `actions` (`id`),
+  CONSTRAINT `fk_role_actions__roles` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `roles` (
+  `id` char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES ('3f321312-4efd-4e8d-a4fe-ba2d9fe9ed84','USER'),('f7030ab4-d20f-469b-8ee4-a1c5e27bee36','ADMIN');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sessions`
+--
+
+DROP TABLE IF EXISTS `sessions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sessions` (
+  `id` char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_id` char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_sessions__users` (`user_id`),
+  CONSTRAINT `fk_sessions__users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `username` varchar(320) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(320) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(320) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lastname` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone` varchar(25) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `profile_photo_url` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_date` datetime NOT NULL,
+  `updated_date` datetime NOT NULL,
+  `deleted_date` datetime DEFAULT NULL,
+  `role_id` char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_users__roles` (`role_id`),
+  CONSTRAINT `fk_users__roles` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
